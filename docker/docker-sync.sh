60aa96bab85a036bbf9c790c9bac9f4610699a33
donghee <dongheepark@gmail.com>
2025-02-01 14:32:41 +0900

    add docker sync

---
+++ docker/docker-sync.sh
@@ -0,0 +1,3 @@
+#mutagen sync create --name=wearable_robot_upper_limb ~/tmp docker://wearable/dynamixel_ws
+mutagen sync create --name=wearable_robot_upper_limb ~/tmp donghee@pi.lan:~/dynamixel_ws
+mutagen sync list
