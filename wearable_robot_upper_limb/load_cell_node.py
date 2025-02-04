138ed5d18a7dc610f149b3b824f83d3cab7603e9
Donghee Park <dongheepark@gmail.com>
2025-02-04 14:44:05 +0900

    update loadcell publish rate to 100hz

--- wearable_robot_upper_limb/load_cell_node.py
+++ wearable_robot_upper_limb/load_cell_node.py
@@ -10,65 +10,84 @@ from std_msgs.msg import Float32
 import RPi.GPIO as GPIO
 from hx711 import HX711
 import time
+import threading
 
 class LoadCellNode(Node):
     def __init__(self):
         super().__init__('load_cell_node')
-        self.publisher_ = self.create_publisher(Float32, 'load_cell_weight', 10)
-        self.timer = self.create_timer(1.0, self.publish_weight)  # Publish every 1 second
-        
+
+        self.hx711 = None
+        self.OFFSET = 0
+        self.SCALE_FACTOR = 279  # Get scale factor from calibration; scale factor 279 borrow from KNU's upper limb arduino firmware
+        self.current_weight = None
+        self.lock = threading.Lock()
+        self.stop_thread = threading.Event()
+
         self.get_logger().info("Initializing load cell...")
-        self.init_load_cell()
+        self.init_calibration()
         self.get_logger().info("Load cell initialized.")
 
-    def init_load_cell(self):
+        self.read_thread = threading.Thread(target=self.read_weight_loop)
+        self.read_thread.start()
+
+        self.publisher_ = self.create_publisher(Float32, 'load_cell_weight', 10)
+        self.timer = self.create_timer(0.01, self.publish_weight)  # Publish every 0.01 second; 100Hz
+        
+    def init_calibration(self):
         # Calibration
-        measures = []
         try:
-            hx711 = HX711(
+            self.hx711 = HX711(
                 dout_pin=5,
                 pd_sck_pin=6,
                 channel='A',
-                gain=64
+                gain=128
             )
-            hx711.reset()
-            measures = hx711.get_raw_data(times=10)
+            self.hx711.reset()
+            measures = self.hx711.get_raw_data(times=10)
+            self.OFFSET = sum(measures) / len(measures)
         finally:
             GPIO.cleanup()
 
-        self.OFFSET = sum(measures) / len(measures)
-        self.SCALE_FACTOR = 100  # DONGHEE
+    def read_weight_loop(self):
+        self.hx711 = HX711(
+            dout_pin=5,
+            pd_sck_pin=6,
+            channel='A',
+            gain=128
+        )
+        self.hx711.reset()
 
-    def read_weight(self):
-        measures = []
-        try:
-            hx711 = HX711(
-                dout_pin=5,
-                pd_sck_pin=6,
-                channel='A',
-                gain=64
-            )
-            hx711.reset()
-            measures = hx711.get_raw_data(times=3)
-        finally:
-            GPIO.cleanup()
-
-        calibrated_measures = [(x - self.OFFSET) / self.SCALE_FACTOR for x in measures]
-        return sum(calibrated_measures) / len(calibrated_measures)
+        while not self.stop_thread.is_set(): 
+            try:
+                measures = self.hx711.get_raw_data(times=3)
+                calibrated_measures = [(x - self.OFFSET) / self.SCALE_FACTOR for x in measures]
+                with self.lock:
+                    self.current_weight = sum(calibrated_measures) / len(calibrated_measures)
+            except Exception as e:
+                self.get_logger().error(e)
+            finally:
+                continue
+                #  print('cleanup')
+        GPIO.cleanup()
 
     def publish_weight(self):
-        weight = self.read_weight()
+        if self.current_weight is None:
+            return
         msg = Float32()
-        msg.data = float(weight)
+        msg.data = float(self.current_weight)
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing weight: {weight:.2f}g')
+        self.get_logger().info(f'Publishing weight: {self.current_weight:.2f}g')
 
 def main(args=None):
     rclpy.init(args=args)
     load_cell_node = LoadCellNode()
-    rclpy.spin(load_cell_node)
-    load_cell_node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(load_cell_node)
+    except KeyboardInterrupt:
+        load_cell_node.stop_thread.set()
+        load_cell_node.read_thread.join()
+        load_cell_node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
