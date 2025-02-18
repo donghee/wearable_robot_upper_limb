29ff22eff8eb04c486349cd4b3adcafb229ea1f2
Donghee Park <dongheepark@gmail.com>
2025-02-18 22:10:15 +0900

    add comments in docker utils

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,12 @@ sudo usermod -aG docker $USER
 
 cd "$(dirname "$0")/.."
 
+# If the machine is x86_64, build for arm64
+if [ "$(uname -m)" = "x86_64" ]; then
+  docker run --privileged --rm tonistiigi/binfmt:qemu-v7.0.0-28 --install arm64,riscv64,arm
+  docker build --platform linux/arm64 -t ghcr.io/donghee/wearable_robot_upper_limb:latest -f docker/Dockerfile .
+  exit 0
+fi
+
+# build in raspberry pi 
 docker build -t ghcr.io/donghee/wearable_robot_upper_limb:latest -f docker/Dockerfile .
--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,15 @@
-# delete previous container
+# stop and delete previous container
 docker stop wearable; docker rm wearable
 
-# running new container
-#docker run -it --name wearable -d --restart unless-stopped --privileged -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v=/dev:/dev -v=$HOME/dynamixel_ws:/dynamixel_ws arm64v8/ros:humble bash -c "sudo apt-get update && sudo apt-get install python3-pip -y && python3 -m pip install rpi-lgpio && python3 -m pip install hx711 && cd /dynamixel_ws/src/wearable_robot_upper_limb/docker/DynamixelSDK-3.8.1/python/ && python3 setup.py install && source /dynamixel_ws/install/setup.bash && ros2 launch wearable_robot_upper_limb robot.launch.py"
+# If the machine is x86_64, run the container with arm64 platform
+if [ "$(uname -m)" = "x86_64" ]; then
+  docker run -it --name wearable --platform linux/arm64 -d --restart unless-stopped --privileged -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v=/dev:/dev -v=$HOME/tmp/dynamixel_ws:/dynamixel_ws ghcr.io/donghee/wearable_robot_upper_limb:latest
+  exit 0
+fi
+
+# running new container in raspberry pi 5
+## auto start robot.launch.py during boot.
 docker run -it --name wearable -d --restart unless-stopped --privileged -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v=/dev:/dev -v=$HOME/dynamixel_ws:/dynamixel_ws ghcr.io/donghee/wearable_robot_upper_limb:latest bash -c "source /opt/ros/humble/setup.bash && source /dynamixel_ws/install/setup.bash && ros2 launch wearable_robot_upper_limb robot.launch.py"
-#docker run -it --name wearable -d --restart unless-stopped --privileged -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v=/dev:/dev -v=$HOME/dynamixel_ws:/dynamixel_ws ghcr.io/donghee/wearable_robot_upper_limb:latest 
 
-
+## manual start
+# docker run -it --name wearable --rm --privileged -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v=/dev:/dev -v=$HOME/dynamixel_ws:/dynamixel_ws ghcr.io/donghee/wearable_robot_upper_limb:latest 
